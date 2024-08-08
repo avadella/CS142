1634cd49f9df2a268772e93c29c64950a7caf7f7
Vadella, Anna <anna.vadella@gmail.com>
2024-08-08 13:04:19 -0400

    Create assignment1

---
+++ Projects/assignment1.cpp
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------
+* Assignment 1: Welcome to Class
+* Developer: Anna Vadella
+* Date: September 6, 2021
+*------------------------------------------------------------*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "-------------------" << endl;
+	cout << "Welcome to CS142, Introduction to Computer Science and Programming! We are going to have a great semester!" << endl;
+	cout << "-------------------" << endl;
+	return 0;
+}
